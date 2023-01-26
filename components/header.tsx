55a6810048e6acc4f7313129a2e470237592e22c
Naoki-Yuzu <toaru.programming@gmail.com>
2023-01-26 17:03:37 +0900

    ユーザーのドロップダウンメニューの表示の実装

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 import Button from './button';
 import Logo from './logo';
+import UserMenu from './user-menu';
 
 const Header = () => {
   return (
@@ -10,13 +11,14 @@ const Header = () => {
       <div className="flex items-center h-full gap-2">
         <Logo />
         <span className="flex-1"/>
-        <Link href="/login" className="text-xs sm:text-base rounded-xl font-semibold border-orange-300 border-2 py-1 px-3 hover:opacity-75 duration-200 tracking-widest text-orange-300 mr-1">ログイン</Link>
-        <Link href="/sign-up" className="text-xs sm:text-base rounded-xl font-semibold border-orange-300 border-2 py-1 px-3 hover:opacity-75 duration-200 tracking-widest text-white bg-orange-300">会員登録</Link>
+        {/* <Link href="/login" className="text-xs sm:text-base rounded-xl font-semibold border-orange-300 border-2 py-1 px-3 hover:opacity-75 duration-200 tracking-widest text-orange-300 mr-1">ログイン</Link>
+        <Link href="/sign-up" className="text-xs sm:text-base rounded-xl font-semibold border-orange-300 border-2 py-1 px-3 hover:opacity-75 duration-200 tracking-widest text-white bg-orange-300">会員登録</Link> */}
         {/* <Button className="text-orange-300 mr-1">ログイン</Button>
         <Button className="text-white bg-orange-300">会員登録</Button> */}
-        <button className="mr-5 hidden">
+        <UserMenu />
+        {/* <button className="mr-5">
           <img className=" rounded-full object-cover w-10 h-10 object-right-top" src="/profile-image.jpg" alt="profileImage"/>
-        </button>
+        </button> */}
         <Button className="hidden text-white bg-orange-300">投稿</Button>
       </div>
     </header>
---
+++ components/user-menu.tsx
@@ -0,0 +1,90 @@
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState, forwardRef, ReactNode } from 'react'
+import { UserIcon, ArrowLeftOnRectangleIcon, ChatBubbleLeftEllipsisIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link';
+
+const LinkForHeadlessUi = forwardRef<HTMLAnchorElement, {
+  href: string;
+  children: ReactNode;
+  className: string | undefined;
+}>((props) => {
+  let { href, children, className, ...rest } = props
+  return (
+    <Link className={className} href={href} {...rest}>
+      {children}
+    </Link>
+  )
+})
+
+LinkForHeadlessUi.displayName = "toProfileLink";
+
+const UserMenu = () => {
+  return (
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="block mr-5">
+            <img className=" rounded-full object-cover w-10 h-10 object-right-top" src="/profile-image.jpg" alt="profileImage"/>
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          {/* <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"> */}
+          <Menu.Items className="shadow-md absolute right-0 mt-1 sm:mt-2 w-56 origin-top-right bg-white border rounded-md flex flex-col">
+            <div className="px-2 py-1 sm:px-3 sm:py-2 border-b h-10 flex items-center gap-1 sm:gap-3">
+              <Menu.Item >
+                {({ active }) => (
+                  <>
+                    <UserIcon className="h-5 w-5"/>
+                    <LinkForHeadlessUi href="/profile" className="block text-xs sm:text-sm w-full font-semibold">
+                      プロフィール
+                    </LinkForHeadlessUi>
+                  </>
+                )}
+              </Menu.Item>
+            </div>
+            <div className="px-2 py-1 sm:px-3 sm:py-2 border-b h-10 flex items-center gap-1 sm:gap-3">
+              <Menu.Item>
+                {({ active }) => (
+                  <>
+                    <ChatBubbleLeftEllipsisIcon className="h-5 w-5"/>
+                    <LinkForHeadlessUi href="/chat" className="block text-xs sm:text-sm w-full font-semibold">
+                      メッセージ管理
+                    </LinkForHeadlessUi>
+                  </>
+                )}
+              </Menu.Item>
+            </div>
+            <div className="px-2 py-1 sm:px-3 sm:py-2 flex items-center h-10 gap-1 sm:gap-3">
+              <Menu.Item>
+                {({ active }) => (
+                  <>
+                    <ArrowLeftOnRectangleIcon className="h-5 w-5"/>
+                    <button className="text-xs sm:text-sm w-full font-semibold text-left">
+                      ログアウト
+                    </button>
+                  </>
+                )}
+              </Menu.Item>
+            </div>
+            
+          </Menu.Items>
+        </Transition>
+      </Menu>
+  )
+}
+
+export default UserMenu;
+
+{/* <button
+                    className={`${
+                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                  </button> */}
